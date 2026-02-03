9f8aa1d8c09c54e2b4da737bb7684718b5bf9500
cadagoberto <jbcristian78@gmail.com>
2026-02-03 20:29:53 -0300

    Criando pagina de produto

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,22 @@
-import { useState, useEffect } from "react"
+
 import './App.css'
-import HeaderLoja from './components/HeaderLoja'
-import SearchBar from './components/SearchBar'
-import { Categoria } from './components/Categoria'
-import BottomNav from './components/BottomNav'
-import MobileNavbar from './components/MobileNavbar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import ProdutoPage from "./pages/ProdutoPage"
+import ButtomNav from "./components/BottomNav"
 
-
- 
 
 export default function App() {
-  const [showNavbar, setShowNavbar] = useState(false)
-  const [isWide, setIsWide] = useState(window.innerWidth > 900)
-
-
-  useEffect(() => {
-  const handleResize = () => setIsWide(window.innerWidth > 900)
-  const handleScroll = () => setShowNavbar(window.scrollY > 100)
-
-  window.addEventListener("resize", handleResize)
-  window.addEventListener("scroll", handleScroll)
-
-  // cleanup
-  return () => {
-    window.removeEventListener("resize", handleResize)
-    window.removeEventListener("scroll", handleScroll)
-  }
-}, [])
-
-  
-const categorias = [
-  {
-    titulo: "Destaques",
-    produtos: [
-      { titulo: "Produto 1", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "3", valorProd: 50.90 },
-      { titulo: "Produto 2", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "4", valorProd: 60.90, img: "assets/img/pratos/sushi.jpg" },
-      { titulo: "Produto 2", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "4", valorProd: 60.90, img: "assets/img/pratos/sushi.jpg" },
-      { titulo: "Produto 2", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "4", valorProd: 60.90, img: "assets/img/pratos/sushi.jpg" }
-    ]
-  },
-  {
-    titulo: "Promoções",
-    produtos: [
-      { titulo: "Produto 3", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "2", valorProd: 30.90 },
-      { titulo: "Produto 4", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "5", valorProd: 40.90 }
-    ]
-  }
-]
+ 
 
   return (
-    <>
-      <div className={` ${isWide ? "container" : ""}`}>
-         <MobileNavbar lojaNome="Minha Loja" categorias={categorias.map(titulo => titulo.titulo)} visible={showNavbar} />
-
-        <div className='header'>
-        <img src="../assets/img/loja/loja1.jpg" alt="" />
-        </div>
-
-        <div className="bodyApp">
-          <div className="container bodyContainer">
-            {/* header loja */}
-            <HeaderLoja nomeLoja={"Nome loja exemplo"} horarioFunc={"exemplo de horario 17h às 22h"}/>
-
-            {/* Pesquisa produtos */}
-            <SearchBar />
-
-            {/* Categorias */}
-            {categorias.map((p, index) => (
-            <Categoria 
-              key={index} 
-              titulo={p.titulo} 
-              produtos={p.produtos} 
-              id={p.titulo}
-            />
-          ))}
-
-            
-          </div>
-
-        </div>
-        <BottomNav />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/produto" element={<ProdutoPage />} />
+      </Routes>
+      <ButtomNav />
+    </Router>
   )
 }
 
---
+++ src/pages/ProdutoPage.jsx
@@ -0,0 +1,82 @@
+
+import { useState, useEffect } from "react"
+import HeaderLoja from '../components/HeaderLoja'
+import SearchBar from '../components/SearchBar'
+import { Categoria } from '../components/Categoria'
+import MobileNavbar from '../components/MobileNavbar'
+
+
+export default function ProdutoPage() {
+const [showNavbar, setShowNavbar] = useState(false)
+      const [isWide, setIsWide] = useState(window.innerWidth > 900)
+    
+    
+      useEffect(() => {
+      const handleResize = () => setIsWide(window.innerWidth > 900)
+      const handleScroll = () => setShowNavbar(window.scrollY > 100)
+    
+      window.addEventListener("resize", handleResize)
+      window.addEventListener("scroll", handleScroll)
+    
+      // cleanup
+      return () => {
+        window.removeEventListener("resize", handleResize)
+        window.removeEventListener("scroll", handleScroll)
+      }
+    }, [])
+
+    const categorias = [
+      {
+        titulo: "Destaques",
+        produtos: [
+          { titulo: "Produto 1", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "3", valorProd: 50.90 },
+          { titulo: "Produto 2", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "4", valorProd: 60.90, img: "assets/img/pratos/sushi.jpg" },
+          { titulo: "Produto 2", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "4", valorProd: 60.90, img: "assets/img/pratos/sushi.jpg" },
+          { titulo: "Produto 2", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "4", valorProd: 60.90, img: "assets/img/pratos/sushi.jpg" }
+        ]
+      },
+      {
+        titulo: "Promoções",
+        produtos: [
+          { titulo: "Produto 3", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "2", valorProd: 30.90 },
+          { titulo: "Produto 4", desc: "Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", qtdPessoas: "5", valorProd: 40.90 }
+        ]
+      }
+    ]
+
+  return (
+    <>
+        <div className={` ${isWide ? "container" : ""}`}>
+                         <MobileNavbar lojaNome="Minha Loja" categorias={categorias.map(titulo => titulo.titulo)} visible={showNavbar} />
+                
+                        <div className='header'>
+                        <img src="../assets/img/loja/loja1.jpg" alt="" />
+                        </div>
+                
+                        <div className="bodyApp">
+                          <div className="container bodyContainer">
+                            {/* header loja */}
+                            <HeaderLoja nomeLoja={"Nome loja exemplo"} horarioFunc={"exemplo de horario 17h às 22h"}/>
+                
+                            {/* Pesquisa produtos */}
+                            <SearchBar />
+                
+                            {/* Categorias */}
+                            {categorias.map((p, index) => (
+                            <Categoria 
+                              key={index} 
+                              titulo={p.titulo} 
+                              produtos={p.produtos} 
+                              id={p.titulo}
+                            />
+                          ))}
+                
+                            
+                          </div>
+                
+                        </div>
+                        
+                      </div>
+    </>
+  )
+}
